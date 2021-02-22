b0c168bbfc8388435cf65eb8f52a9d29b03e9862
Antoine RIQUET <antoine.riquet01@orange.fr>
2021-02-22 17:37:02 +0100

    inventaire fait

---
+++ Hanabi/Assets/Test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+     public void item0()
+     {
+          InventairePassif.instance.Item0();
+     }
+}
