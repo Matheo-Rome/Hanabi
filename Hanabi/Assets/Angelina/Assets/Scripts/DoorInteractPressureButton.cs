18eee5b02f9ce7c4a1adaea86dee940042a7d04b
Angelina <79404132+anglnn@users.noreply.github.com>
2021-02-23 19:52:03 +0100

    jai encore un pb avec player movement du coup je repull

---
+++ Hanabi/Assets/Angelina/Assets/Scripts/DoorInteractPressureButton.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteractPressureButton : MonoBehaviour
+{
+
+   /* [SerializeField] private GameObject doorGameObject;
+
+    private IDoor door;
+    private float timer;
+
+    private void Awake()
+    {
+        door = doorGameObject.GetComponent<IDoor>();
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                door.CloseDoor();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            door.OpenDoor();
+            timer = 5f;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            timer = 1f;
+        }
+    }*/
+   
+   
+   private PlayerMovement player;
+
+   //portefermée
+   public SpriteRenderer theSR;
+   //porte ouverte
+   public Sprite doorOpenSprite;
+    
+
+   public bool doorOpen, waitingToOpen;
+   private float timer;
+
+   void Start()
+   {
+       player = FindObjectOfType<PlayerMovement>();
+   }
+
+   private void Update()
+   {
+       if (timer > 0)
+       {
+           timer -= Time.deltaTime;
+           if (timer <= 0f)
+           {
+               gameObject.SetActive(true);
+           }
+       }
+   }
+
+   private void OnTriggerEnter2D(Collider2D collider)
+   {
+       if (collider.CompareTag("Player"))
+       {
+           timer = 5f;
+           doorOpen = true;
+           
+           theSR.sprite = doorOpenSprite;
+           
+           gameObject.SetActive(false);
+       }
+   }
+   
+}
