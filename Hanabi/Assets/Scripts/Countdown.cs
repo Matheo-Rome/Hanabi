1caeecdd686d2dffcf3b7b36de976935449b2c93
Hugo Schlegel <hugo.schlegel@epita.fr>
2021-02-25 20:28:12 +0100

    started working on the item timer

---
+++ Hanabi/Assets/Scripts/Countdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    public float timeStart;
+    public Text textBox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeStart > 0)
+        {
+            timeStart -= Time.deltaTime;
+            textBox.text = Mathf.Round(timeStart).ToString();
+        }
+
+        if (timeStart < 0)
+        {
+            textBox.text = "Ready";
+        }
+    }
+}
