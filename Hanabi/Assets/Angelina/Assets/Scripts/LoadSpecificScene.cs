509b1978c21f40de287102ca2f54f3bb4eeb3090
Angelina <79404132+anglnn@users.noreply.github.com>
2021-02-28 16:54:53 +0100

    ajout de toute les scènes qui se suivent

--- Hanabi/Assets/Angelina/Assets/Scripts/LoadSpecificScene.cs
+++ Hanabi/Assets/Angelina/Assets/Scripts/LoadSpecificScene.cs
@@ -1,13 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadSpecificScene : MonoBehaviour
 {
+    private int i = 1;
+    private int y;
+    private bool incrementation = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(1/*"z1_boutique"*/);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
+    
 }
