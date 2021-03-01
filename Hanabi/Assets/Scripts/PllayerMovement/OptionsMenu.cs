8efb873fae376c03e2fb04e2890ebc81538a8355
Hugo Schlegel <hugo.schlegel@epita.fr>
2021-03-01 17:53:18 +0100

    started working on the options menu

---
+++ Hanabi/Assets/Scripts/PllayerMovement/OptionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
