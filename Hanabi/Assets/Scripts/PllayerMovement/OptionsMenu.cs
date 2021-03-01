5ee8d6427ee96aab6cacc8904977e613eb2e05e7
Hugo Schlegel <hugo.schlegel@epita.fr>
2021-03-01 18:49:09 +0100

    👉👈 le menu is for me ?

--- Hanabi/Assets/Scripts/PllayerMovement/OptionsMenu.cs
+++ Hanabi/Assets/Scripts/PllayerMovement/OptionsMenu.cs
@@ -17,4 +17,9 @@ public class OptionsMenu : MonoBehaviour
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 }
