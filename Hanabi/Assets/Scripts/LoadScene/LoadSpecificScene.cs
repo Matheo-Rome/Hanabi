42828b125196b8f3501772a16f3ab3bb619f5324
Angelina <79404132+anglnn@users.noreply.github.com>
2021-03-03 20:09:42 +0100

    script clean + reaparition des musiques

--- Hanabi/Assets/Scripts/LoadScene/LoadSpecificScene.cs
+++ Hanabi/Assets/Scripts/LoadScene/LoadSpecificScene.cs
@@ -4,10 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class LoadSpecificScene : MonoBehaviour
 {
-    private int i = 1;
-    private int y;
-    private bool incrementation = false;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
