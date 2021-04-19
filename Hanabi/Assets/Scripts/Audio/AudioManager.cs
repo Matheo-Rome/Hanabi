e34a64e4d328a81eb17b497731bf3217934764ce
Antoine0871 <antoine.riquet01@orange.fr>
2021-04-19 17:13:26 +0200

    DIsorsion + blur1

--- Hanabi/Assets/Scripts/Audio/AudioManager.cs
+++ Hanabi/Assets/Scripts/Audio/AudioManager.cs
@@ -1,22 +1,45 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 { 
     public AudioClip[] playlist; 
     public AudioSource audioSource;
     private int musicIndex = 0;
-    
+    public AudioDistortionFilter _audioDistortionFilter;
     void Start()
     {
         audioSource.clip = playlist[0];
         audioSource.Play();
+        if(PlayerStress.instance.currentStress >= 100)
+        {
+            _audioDistortionFilter.distortionLevel = (float)0.85;
+            RenderSettings.ambientLight = Color.black;
+        }
+        if(PlayerStress.instance.currentStress < 100)
+        {
+            _audioDistortionFilter.distortionLevel = (float)0.5;
+        }
     }
 
-    void Update()
+    void Update()   
     {
         if (!audioSource.isPlaying)
         {
             PlayNextSong();
+            if(PlayerStress.instance.currentStress >= 100)
+            {
+                _audioDistortionFilter.distortionLevel = (float)1;
+            }
+        }
+        if(PlayerStress.instance.currentStress >= 100)
+        {
+            _audioDistortionFilter.distortionLevel = (float)0.85;
+            RenderSettings.ambientLight = Color.black;
+        }
+        if(PlayerStress.instance.currentStress < 100)
+        {
+            _audioDistortionFilter.distortionLevel = (float)0.5;
         }
     }
 
@@ -25,6 +48,15 @@ public class AudioManager : MonoBehaviour
         musicIndex = (musicIndex + 1) % playlist.Length;
         audioSource.clip = playlist[musicIndex];
         audioSource.Play();
+        if(PlayerStress.instance.currentStress >= 100)
+        {
+            _audioDistortionFilter.distortionLevel = (float)0.85;
+            RenderSettings.ambientLight = Color.black;
+        }
+        if(PlayerStress.instance.currentStress < 100)
+        {
+            _audioDistortionFilter.distortionLevel = (float)0.5;
+        }
     }
     
 }
