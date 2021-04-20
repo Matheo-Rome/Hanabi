2cc2314698c3da7dd2bdc4181a6b5003c8288290
matheo.rome <matheo.rome@epita.fr>
2021-04-20 21:24:04 +0200

    bruh

---
+++ Hanabi/Hanabi/Assets/Scripts/Item/PickUpItem.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpItem : MonoBehaviour
+{
+    private Text interactUI;
+    private bool isInRange;
+
+    public Items item;
+
+    void Awake()
+    {
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isInRange)
+        {
+            TakeItem();
+        }
+    }
+
+    void TakeItem()
+    {
+        inventory.instance.remplacementItem(item);
+        inventory.instance.updateinventoryImage();
+        interactUI.enabled = false;
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            interactUI.enabled = true;
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            interactUI.enabled = false;
+            isInRange = false;
+        }
+    }
+}
