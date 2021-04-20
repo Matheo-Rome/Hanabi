c5c8c04b4842544c7f42c16fab14a6ca55473f3f
matheo.rome <matheo.rome@epita.fr>
2021-04-20 20:38:33 +0200

    bruh

---
+++ Hanabi/Hanabi/Assets/Scripts/ButtonDoor/DoorSetActive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSetActive : MonoBehaviour
+{
+    public void OpenDoor()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void CloseDoor()
+    {
+        gameObject.SetActive(true);
+    }
+    
+}
