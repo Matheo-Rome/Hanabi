a604468d008414db9980f27b2e5f79e3f783efe9
Antoine0871 <antoine.riquet01@orange.fr>
2021-04-21 16:45:03 +0200

    IA se supprime a 189 pts de stresse

--- Hanabi/Assets/InstantiateFantome.cs
+++ Hanabi/Assets/InstantiateFantome.cs
@@ -7,22 +7,23 @@ public class InstantiateFantome : MonoBehaviour
     public bool Oneinstance = false;
     
     public GameObject Fantome;
+    public GameObject instance;
     void Start()
     {
         
     }
-
     void Update()
     {
         if (PlayerStress.instance.currentStress == 200 && !Oneinstance)
         {
-            Instantiate(Fantome, transform.position, Quaternion.identity);
+            instance = Instantiate(Fantome, transform.position, Quaternion.identity);
             Oneinstance = true;
         }
 
-        if (PlayerStress.instance.currentStress < 200)
+        if (PlayerStress.instance.currentStress <= 189 && Oneinstance)
         {
             Oneinstance = false;
+            Destroy(instance);
         }
     }
 }
