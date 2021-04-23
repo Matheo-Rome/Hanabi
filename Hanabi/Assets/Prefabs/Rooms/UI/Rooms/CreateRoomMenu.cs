9a14cbc507efb9925f52d3f5d4d1b3c027051088
matheo.rome <matheo.rome@epita.fr>
2021-04-23 23:06:04 +0200

    menu multi

---
+++ Hanabi/Assets/Prefabs/Rooms/UI/Rooms/CreateRoomMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField] 
+    private Text _roomName;
+
+    public void OnClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options,TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+       Debug.Log("Created room successfully");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed " + message);
+    }
+}
---
+++ Hanabi/Assets/Prefabs/Rooms/UI/Rooms/TestConnect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{ 
+    
+    private void Start()
+    {
+        print("Connectiong to server.");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connectiong to server.");
+        print(PhotonNetwork.LocalPlayer.NickName);
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from server for reason" + cause.ToString());
+    }
+}
