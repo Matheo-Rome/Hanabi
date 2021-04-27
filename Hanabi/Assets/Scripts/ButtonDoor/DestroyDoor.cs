1c069eb9bc046ba07053bce7853e00fa109848c7
Angelina <79404132+anglnn@users.noreply.github.com>
2021-04-27 13:33:37 +0200

    je pense pas que ça marche

---
+++ Hanabi/Assets/Scripts/ButtonDoor/DestroyDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDoor : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Destroy(GetComponent<BoxCollider2D>());
+        }
+    }
+}
