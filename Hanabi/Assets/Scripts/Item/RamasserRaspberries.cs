ff1a4fd8b63e363db2dfffb7c7f5212782bf2767
matheo.rome <matheo.rome@epita.fr>
2021-06-16 13:55:53 +0200

    save good

--- Hanabi/Assets/Scripts/Item/RamasserRaspberries.cs
+++ Hanabi/Assets/Scripts/Item/RamasserRaspberries.cs
@@ -1,17 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class RamasserRaspberries : MonoBehaviour
+public class RamasserRaspberries : MonoBehaviourPunCallbacks
 {
+    private GameObject p1;
+    private GameObject p2;
+    private bool founded = false;
+    
+    private void Update()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            if (!founded)
+            {
+                p1 = GameObject.FindGameObjectWithTag("Inventaire");
+                p2 = GameObject.FindGameObjectWithTag("Inventaire2");
+                
+                founded = p1 != null && p2 != null;
+            }
+
+        }
+    }
+
     //Si le joueur rentre dans la Raspberries, elle s'enlève
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player"))
+        if (col.CompareTag("Player") || col.CompareTag("Player1") || col.CompareTag("Player2"))
         {
-            inventory.instance.AddRaspberries(1,true);
-            Destroy(gameObject);
+            /*inventory.instance.AddRaspberries(1,true);
+            Destroy(gameObject);*/
+            
+            if (!PhotonNetwork.IsConnected)
+            {
+                p1.GetComponent<inventory>().AddRaspberries(1,false);
+                p2.GetComponent<inventory>().AddRaspberries(1,false);
+                Destroy(gameObject);
+            }
+            else
+            {
+                if (photonView.IsMine)
+                {
+                    Debug.Log("dsskljgmlghjfdslmhkjfdlmksjhf");
+                    GameObject[] inventories = GameObject.FindGameObjectsWithTag("InventaireM");
+                    foreach (var inventory in inventories)
+                    {
+                        inventory.GetComponent<inventory>().AddRaspberries(1, true);
+                    }
+                }
+
+                Destroy(gameObject);
+            }
+
         }
     }
 }
