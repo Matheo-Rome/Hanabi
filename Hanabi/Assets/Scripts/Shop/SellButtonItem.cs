58a212127c3a421525ab0c8426aed63a7412e74f
matheo.rome <matheo.rome@epita.fr>
2021-06-17 21:15:39 +0200

    shop solo

--- Hanabi/Assets/Scripts/Shop/SellButtonItem.cs
+++ Hanabi/Assets/Scripts/Shop/SellButtonItem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class SellButtonItem : MonoBehaviour
@@ -9,19 +10,24 @@ public class SellButtonItem : MonoBehaviour
     public Items item;
 
     int reduction = 0;
-    int nouveauxprix = 0;
+    private int nouveauxprix = 0;
+
     public void BuyItem()
     {
-        if (inventory.instance.NombreDePièce >= item.Price)
+        int nbPiece = 0;
+        if (PhotonNetwork.IsConnected)
+            nbPiece = GameObject.FindGameObjectWithTag("InventaireM").GetComponent<inventory>().NombreDePièce;
+        else
+            nbPiece = GameObject.FindGameObjectWithTag("Inventaire").GetComponent<inventory>().NombreDePièce;
+        if (/*inventory.instance.NombreDePièce*/nbPiece >= item.Price )
         {
-
             reduction = 0;
             nouveauxprix = 0;
-
             foreach (var objet in InventairePassif.instance.content)
             {
                 reduction += objet.ReducePrice;
             }
+
 
             nouveauxprix = item.Price - reduction;
             if (nouveauxprix < 0)
@@ -29,12 +35,40 @@ public class SellButtonItem : MonoBehaviour
                 nouveauxprix = 0;
             }
 
-            InventairePassif.instance.content.Add(item);
-            InventairePassif.instance.Start();
-            InventairePassif.instance.AddEffectItem(item);
-            inventory.instance.NombreDePièce -= nouveauxprix;
-            inventory.instance.UpdateTextUI();
-            
+            if (PhotonNetwork.IsConnected)
+            {
+                GameObject[] IPs = GameObject.FindGameObjectsWithTag("IP");
+                GameObject[] Invs = GameObject.FindGameObjectsWithTag("InventaireM");
+                foreach (var IP in IPs)
+                {
+                    InventairePassif ip = IP.GetComponent<InventairePassif>();
+                    ip.content.Add(item);
+                    ip.Start();
+                    ip.AddEffectItem(item, true);
+                }
+
+                foreach (var inv in Invs)
+                {
+                    inv.GetComponent<inventory>().Addcoins(-nouveauxprix, true);
+                }
+            }
+            else
+            {
+                InventairePassif IP1 = GameObject.FindGameObjectWithTag("IP").GetComponent<InventairePassif>();
+                InventairePassif IP2 = GameObject.FindGameObjectWithTag("IP2").GetComponent<InventairePassif>();
+                inventory I1 = GameObject.FindGameObjectWithTag("Inventaire").GetComponent<inventory>();
+                inventory I2 = GameObject.FindGameObjectWithTag("Inventaire2").GetComponent<inventory>();
+                IP1.content.Add(item);
+                IP2.content.Add(item);
+                IP1.Start();
+                IP2.Start();
+                IP1.AddEffectItem(item, true);
+                I1.Addcoins(-nouveauxprix, false);
+                I2.Addcoins(-nouveauxprix, false);
+            }
+
+            //item.Available = false;
         }
     }
 }
+
