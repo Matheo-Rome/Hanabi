f3bd8f3e8048da92fc9db8af7eab03a45ad6aa03
Angelina <79404132+anglnn@users.noreply.github.com>
2021-06-17 20:25:45 +0200

    écran de crédit

--- Hanabi/Assets/Scripts/LoadScene/Load_credit.cs
+++ Hanabi/Assets/Scripts/LoadScene/Load_credit.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using Photon.Realtime;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Photon.Pun;
 
-public class tp_to_z2 : MonoBehaviour
+public class Load_credit : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("Player1") || collision.CompareTag("Player2"))
         {
             new WaitForSeconds(0.3f);
-            PhotonNetwork.LoadLevel(15);
+            PhotonNetwork.LoadLevel(65);
         }
-        
+                
     }
-
 }
---
+++ Hanabi/Assets/Scripts/LoadScene/crédit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class crédit : MonoBehaviour
+{
+    void LoadMenu()
+    {
+        PhotonNetwork.LoadLevel(0);  
+    }
+}
