0e170c1451e3b6ebaa69605b4ee9b9072e77814e
Hugo Schlegel <hugo.schlegel@epita.fr>
2021-06-17 15:21:51 +0200

    finished the last update

--- Hanabi/Assets/Scripts/Upgrades/upgradesInventory.cs
+++ Hanabi/Assets/Scripts/Upgrades/upgradesInventory.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Photon.Pun;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class upgradesInventory : MonoBehaviour
 {
     public static upgradesInventory instance;
     public List<upgradesSO> content = new List<upgradesSO>(); // Liste où sont stocké tous les items
+    public List<Items> items = new List<Items>();
     
-    /*private bool founded = false;
+    private bool founded = false;
     private GameObject p1;
-    private GameObject p2;*/
+    private GameObject p2;
     private void Awake()
     {
         if (instance != null)
@@ -45,94 +47,104 @@ public class upgradesInventory : MonoBehaviour
         {
             ValueOfUpgrade.instance.AmeliorationStress += 20;
         }
-        
-    /*//test
-    PlayerMovement.instance.jumpVelocity += Upgrade.jumpBoostGiven;
+            
+        //test
+        PlayerMovement.instance.jumpVelocity += Upgrade.jumpBoostGiven;
 
-    // Pour la jar
-    ValueOfUpgrade.instance.addGiventByJar += Upgrade.coinDropUpgrade;
-    
-    //Pour le Oscillococcinum
-    if (!PhotonNetwork.IsConnected)
-    {
-        p1.GetComponent<PlayerStressSolo>().maxStress += Upgrade.addMaxStress;
-        p2.GetComponent<PlayerStressSolo>().maxStress += Upgrade.addMaxStress;
-    }
-    
-    else
-    {
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        foreach (var player in players)
+        // Pour la jar
+        ValueOfUpgrade.instance.AmeliorationJar += Upgrade.coinDropUpgrade;
+
+        //Pour le Oscillococcinum
+        if (!PhotonNetwork.IsConnected)
         {
-            player.GetComponent<PlayerStress>().UpdateMaxStress(Upgrade.addMaxStress);
+            p1.GetComponent<PlayerStressSolo>().maxStress += Upgrade.addMaxStress;
+            p2.GetComponent<PlayerStressSolo>().maxStress += Upgrade.addMaxStress;
         }
-    }
-    
-    //Pour la banque
-    GameObject[] packages = GameObject.FindGameObjectsWithTag("Package");
-    foreach (var package in packages)
-    {
-        package.GetComponent<SaveData>().UpdateBank(Upgrade.addMaxBank,true);
-    }
-    
-    //Pour le feu de camp
-    if (PhotonNetwork.IsConnected)
-    {
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        players[0].GetComponent<PlayerStress>().firecampValue += Upgrade.FireCamp;
-        int saveStress = (int)(players[0].GetComponent<PlayerStress>().currentStress * (players[0].GetComponent<PlayerStress>().firecampValue));
-        foreach (var player in players)
+
+        else
         {
-            player.GetComponent<PlayerStress>().HealStressplayer(player.GetComponent<PlayerStress>().currentStress - saveStress);
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (var player in players)
+            {
+                player.GetComponent<PlayerStress>().UpdateMaxStress(Upgrade.addMaxStress);
+            }
+        }
+
+        //Pour la banque
+        GameObject[] packages = GameObject.FindGameObjectsWithTag("Package");
+        foreach (var package in packages)
+        {
+            package.GetComponent<SaveData>().UpdateBank(Upgrade.addMaxBank,true);
+        }
+
+        //Pour le feu de camp
+        if (PhotonNetwork.IsConnected)
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            players[0].GetComponent<PlayerStress>().firecampValue += Upgrade.FireCamp;
+            int saveStress = (int)(players[0].GetComponent<PlayerStress>().currentStress * (players[0].GetComponent<PlayerStress>().firecampValue));
+            foreach (var player in players)
+            {
+                player.GetComponent<PlayerStress>().HealStressplayer(player.GetComponent<PlayerStress>().currentStress - saveStress);
+            }
+
+            //Random.org
+            var rd = new Random();
+            int[] broken = new[] {16, 9, 4, 20, 12, 1};
+            int[] used = new[] {17, 11, 5, 21, 13, 2};
+            int[] fresh = new[] {18, 10, 6, 22, 14, 3};
+            var nb1 = rd.Next(6);
+            var nb2 = rd.Next(6);
+            switch (Upgrade.givenObjectLevel)
+            {
+                case 1 :
+                    var item1 = items[broken[nb1] - 1];
+                    InventairePassif.instance.content.Add(item1);
+                    InventairePassif.instance.Start();
+                    InventairePassif.instance.AddEffectItem(item1);
+                    break;
+                case 2 :
+                    var item2 = items[used[nb1] - 1];
+                    InventairePassif.instance.content.Add(item2);
+                    InventairePassif.instance.Start();
+                    InventairePassif.instance.AddEffectItem(item2);
+                    break;
+                case 3 :
+                    var item3 = items[fresh[nb1] - 1];
+                    InventairePassif.instance.content.Add(item3);
+                    InventairePassif.instance.Start();
+                    InventairePassif.instance.AddEffectItem(item3); 
+                    break;
+                case 4 : 
+                    var item4 = items[broken[nb1] - 1];
+                    var item5 = items[broken[nb1] - 1];
+                    InventairePassif.instance.content.Add(item4);
+                    InventairePassif.instance.content.Add(item5);
+                    InventairePassif.instance.Start();
+                    InventairePassif.instance.AddEffectItem(item4);
+                    InventairePassif.instance.content.Add(item5);
+                    break;
+                case 5 :
+                    var item6 = items[broken[nb1] - 1];
+                    var item7 = items[broken[nb1] - 1];
+                    InventairePassif.instance.content.Add(item6);
+                    InventairePassif.instance.content.Add(item7);
+                    InventairePassif.instance.Start();
+                    InventairePassif.instance.content.Add(item6);
+                    InventairePassif.instance.AddEffectItem(item7);
+                    break;
+            }
         }
         
-        //Random.org
-        int[] broken = new[] {16, 9, 4, 20, 12, 1};
-        int[] used = new[] {17, 11, 5, 21, 13, 2};
-        int[] fresh = new[] {18, 10, 6, 22, 14, 3};
-        switch (Upgrade.givenObjectLevel)
+        else 
         {
-            case 1 :
-                var item1 = ScriptableObject.CreateInstance<Items>();
-                InventairePassif.instance.content.Add(item1);
-                InventairePassif.instance.Start();
-                InventairePassif.instance.AddEffectItem(item1);
-                break;
-            case 2 :
-                var item2 = ScriptableObject.CreateInstance<Items>();
-                InventairePassif.instance.content.Add(item2);
-                InventairePassif.instance.Start();
-                InventairePassif.instance.AddEffectItem(item2);
-                break;
-            case 3 :
-                var item3 = ScriptableObject.CreateInstance<Items>();
-                InventairePassif.instance.content.Add(item3);
-                InventairePassif.instance.Start();
-                InventairePassif.instance.AddEffectItem(item3);
-                break;
-            case 4 :
-                var item4 = ScriptableObject.CreateInstance<Items>();
-                InventairePassif.instance.content.Add(item4);
-                InventairePassif.instance.Start();
-                InventairePassif.instance.AddEffectItem(item4);
-                break;
-            case 5 :
-                var item5 = ScriptableObject.CreateInstance<Items>();
-                InventairePassif.instance.content.Add(item5);
-                InventairePassif.instance.Start();
-                InventairePassif.instance.AddEffectItem(item5);
-                break;
+            int saveStress = (int)(p1.GetComponent<PlayerStress>().currentStress * (p1.GetComponent<PlayerStress>().firecampValue));
+            p1.GetComponent<PlayerStress>().HealStressplayer(p1.GetComponent<PlayerStress>().currentStress - saveStress);
+            p2.GetComponent<PlayerStress>().HealStressplayer(p2.GetComponent<PlayerStress>().currentStress - saveStress);
         }
+    
     }
-
-    else
-    {
-        int saveStress = (int)(p1.GetComponent<PlayerStress>().currentStress * (p1.GetComponent<PlayerStress>().firecampValue));
-        p1.GetComponent<PlayerStress>().HealStressplayer(p1.GetComponent<PlayerStress>().currentStress - saveStress);
-        p2.GetComponent<PlayerStress>().HealStressplayer(p2.GetComponent<PlayerStress>().currentStress - saveStress);
-    }*/
-    }
-    /*public void Update()
+    public void Update()
     {
         if (!PhotonNetwork.IsConnected)
         {
@@ -144,6 +156,6 @@ public class upgradesInventory : MonoBehaviour
                 founded = p1 != null && p2 != null;
             }
         }
-    }*/
+    }
 }
         
