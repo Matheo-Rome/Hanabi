baaef77e372df143c52750cdf057b3fae447ca99
Angelina <79404132+anglnn@users.noreply.github.com>
2021-06-17 21:24:18 +0200

    fe

---
+++ Hanabi/Assets/Scripts/LoadScene/exit_easter_egg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class exit_easter_egg : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("Player1") || collision.CompareTag("Player2"))
+        {
+            new WaitForSeconds(0.3f);
+            PhotonNetwork.LoadLevel(14);
+        }
+        
+    }
+}
